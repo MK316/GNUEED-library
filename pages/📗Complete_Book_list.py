b62aa9bf0e94ee595a37d28d7b134dbce423d14f
MK <99416359+MK316@users.noreply.github.com>
2025-03-17 17:12:46 +0900

    Update 📗Complete_Book_list.py

--- pages/📗Complete_Book_list.py
+++ pages/📗Complete_Book_list.py
@@ -1,1 +1,24 @@
 import streamlit as st
+import pandas as pd
+
+# Load data
+df = pd.read_csv('pages/gnueed-booklist.csv', encoding='utf-8')
+
+def main():
+    st.title('English Education Library Book Catalog')
+    st.write("Welcome to the English Education Library Book Search Tool! This application allows you to explore our collection of books, sorted either by ID or by title.")
+
+    # User choice for sorting
+    sort_choice = st.radio("Sort books by:", ('ID', 'Book Title'))
+
+    if sort_choice == 'ID':
+        sorted_df = df.sort_values(by='ID')
+    else:
+        sorted_df = df.sort_values(by='Book_Title')
+
+    # Display the DataFrame with a scrollbar
+    st.dataframe(sorted_df, height=300)  # You can adjust the height as needed
+
+if __name__ == "__main__":
+    main()
+import streamlit as st
