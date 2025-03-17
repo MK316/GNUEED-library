5472b8051e529b1007556606816c91b1faafdd41
MK <99416359+MK316@users.noreply.github.com>
2025-03-18 03:31:16 +0900

    Update 📮_Message_Board.py

--- pages/📮_Message_Board.py
+++ pages/📮_Message_Board.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.header("🐾 Message to leave?")
+st.header("📨 Message to leave?")
 st.markdown("+ [This Padlet](https://padlet.com/mirankim316/s25_dlee) serves as a message board to communicate. You can leave messages on the page regarding errors, suggestions, greetings, etc. No login required.")
 st.components.v1.iframe("https://padlet.com/mirankim316/s25_dlee", width=700, height=800)
