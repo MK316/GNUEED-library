d685c33387f40a42e070b22d6e71c885889df71e
MK <99416359+MK316@users.noreply.github.com>
2025-03-17 17:31:06 +0900

    Update 🌱_Introduction_and_How-to.py

--- pages/🌱_Introduction_and_How-to.py
+++ pages/🌱_Introduction_and_How-to.py
@@ -1,14 +1,15 @@
 import streamlit as st
 
 def main():
-    st.title('English Education Department Library')
+    st.markdown('### 🍃 English-Language Books Library (GNU EED)')
+
 
     st.write("""
-    ## Welcome to Our Library!
+    ## 📗 Welcome to Our Library!
     Our Department of English Education has a library dedicated to providing English books for student reading. Below are the methods available to access the books in our library:
     """)
 
-    st.header("How to Use the Library")
+    st.header("📙 How to Use the Library")
     st.write("""
     ### 1. Visit in Person
     - **Direct Selection**: You can visit the library in person and choose the books you want directly from the shelves.
@@ -17,13 +18,13 @@ def main():
     - **Search by ID**: Use this search engine to find the ID of the book you are interested in. After finding the ID, visit the library to locate the book by its specified ID.
     """)
 
-    st.header("Reading and Borrowing Books")
+    st.header("📘 Reading and Borrowing Books")
     st.write("""
     - **Reading Onsite**: You are welcome to read the books at the library.
     - **Borrowing Books**: If you wish to take books home, please visit the department office to fill out the borrowing ledger.
     """)
 
-    st.header("Returning Books")
+    st.header("📕 Returning Books")
     st.write("""
     - **Book Returns**: When returning books, make sure to check them in with the department assistant to ensure they are properly returned to the library system.
     """)
