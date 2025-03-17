bce5db52f3dfef02da47b728532d0fafea40b4c5
MK <99416359+MK316@users.noreply.github.com>
2025-03-17 17:56:00 +0900

    Update 🌱_Introduction_and_How-to.py

--- pages/🌱_Introduction_and_How-to.py
+++ pages/🌱_Introduction_and_How-to.py
@@ -16,7 +16,17 @@ def main():
 
     #### 2. Online Search
     - **Search by ID**: Use this search engine to find the ID of the book you are interested in. After finding the ID, visit the library to locate the book by its specified ID.
+    - **Check availability**: You can check the availability status of the book you wish to borrow by pressing the button below.
     """)
+    st.caption("This Status button can also be found on other pages.")
+    # Link to Google Drive File
+    file_url = "https://docs.google.com/spreadsheets/d/1wVjJ0kT3eRXC_-1rZ1CQUTrwwCT6TJC5UUuaIQUsiMM/edit?usp=sharing"  # Replace YOUR_FILE_ID with your actual file ID
+    st.markdown(
+        f"<a href='{file_url}' target='_blank' style='display: inline-block; text-align: center; "
+        f"background-color: #4CAF50; color: white; padding: 10px 20px; "
+        f"text-decoration: none; border-radius: 4px; margin-top: 10px;'>"
+        f"Status: Check Availability</a>", unsafe_allow_html=True)
+    
     st.markdown("---")
     st.markdown("#### 🔸 Reading and Borrowing Books")
     st.write("""
