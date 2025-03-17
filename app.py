3ef3dcdb3943743f4e2558de4c1ecf3efba9b979
MK <99416359+MK316@users.noreply.github.com>
2025-03-17 16:59:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 
 url = "https://github.com/MK316/GNUEED-library/raw/main/pages/gnueedlibarylogo.png"
-st.image(url, width=400)
+st.image(url, width=600)
 st.caption('"Books are a uniquely portable magic." – Stephen King')
