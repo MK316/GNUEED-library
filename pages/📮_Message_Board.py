461f8c1d079efe2c505c4b8291ee37ba2517d57b
MK <99416359+MK316@users.noreply.github.com>
2025-03-18 03:16:19 +0900

    Create 📮_Message_Board.py

---
+++ pages/📮_Message_Board.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.header("🐾 Files to share: on Padlet")
+st.markdown("+ [This Padlet](https://padlet.com/mirankim316/s25_dlee) serves as a dynamic hub for our Acoustics course. Here, you'll find additional course materials, additional reading resources, and online tools. It's also a space for sharing files and submitting assignments.")
+st.components.v1.iframe("https://padlet.com/mirankim316/s25_dlee", width=700, height=800)
