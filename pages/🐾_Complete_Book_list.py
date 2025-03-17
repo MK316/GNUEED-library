7afac341c09d3b21ac967eb71f873c707efdff38
MK <99416359+MK316@users.noreply.github.com>
2025-03-17 17:19:11 +0900

    Update 🐾_Complete_Book_list.py

--- pages/🐾_Complete_Book_list.py
+++ pages/🐾_Complete_Book_list.py
@@ -6,7 +6,6 @@ df = pd.read_csv('pages/gnueed-booklist.csv', encoding='utf-8')
 
 def main():
     st.markdown('### 📃 English Education Library Book Catalog')
-    st.write("Welcome to the English Education Library Book Search Tool!")
     st.caption("This application allows you to explore our collection of books, sorted either by ID, title, or author name.")
     st.markdown("---")
     # User choice for sorting
