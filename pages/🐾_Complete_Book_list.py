0de2ab34cd4d24451247a952729fdc70aa5ae7cd
MK <99416359+MK316@users.noreply.github.com>
2025-03-17 17:46:10 +0900

    Update 🐾_Complete_Book_list.py

--- pages/🐾_Complete_Book_list.py
+++ pages/🐾_Complete_Book_list.py
@@ -21,6 +21,13 @@ def main():
     # Display the DataFrame with a scrollbar
     st.dataframe(sorted_df, height=300)  # You can adjust the height as needed
 
+    # Link to Google Drive File
+    file_url = "https://docs.google.com/spreadsheets/d/1wVjJ0kT3eRXC_-1rZ1CQUTrwwCT6TJC5UUuaIQUsiMM/edit?usp=sharing"  # Replace YOUR_FILE_ID with your actual file ID
+    if st.button('Download Shared File'):
+        # This is a button that, when clicked, will take the user to the Google Drive file.
+        st.markdown(f"[Click here to download the shared file]({file_url})", unsafe_allow_html=True)
+
+
 if __name__ == "__main__":
     main()
 
